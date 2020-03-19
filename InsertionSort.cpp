ca538915e9df2343fbfe6d23e843d3044d5092d6
1llum1nati <43194859+1llum1nati@users.noreply.github.com>
2020-03-19 23:05:59 +0300

    Add files via upload

---
+++ InsertionSort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <random>
+
+template<typename T>
+void CountingSort(int len, T arr) {
+    for(int i = 1; i < len; i++)
+        for(int j = i; j > 0 && arr[j-1] > arr[j]; j--)
+            std::swap(arr[j-1], arr[j]);
+}
+
+int main() {
+    std::random_device gen;
+    std::uniform_real_distribution<> uid(1, 40);
+    int size = uid(gen);
+    int arr[size];
+    for(int i = 0; i != size; ++i)
+        arr[i] = uid(gen);
+
+    for(auto &b: arr)
+        std::cout << b << " ";
+    std::cout << '\n';
+
+    CountingSort(size, arr);
+
+    for(auto &b: arr)
+        std::cout << b << " ";
+}
